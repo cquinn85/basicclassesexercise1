d929067f861c1e1db2e88b9ba43a93bcaff0c292
cquinn85 <cequinn300@gmail.com>
2020-11-09 18:19:33 -0600

    classes exercise 1

---
+++ BasicClassesExercise/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+        public Car() //constructor special member method has same name as its class
+        {
+
+        }
+
+        //Create a Make property of type string that is public
+        public string Make { get; set; } // get = read, set = write
+                                         //Create a Model property of type string that is public
+        public string Model { get; set; }
+        //Create a Year property of type int that is public
+        public int Year { get; set; }
+        //above properties are what DEFINE a car (MAKE MODEL YEAR)
+
+        //scope: contains constructors
+
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+    }
+}
---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of the Car class
+            var myCar = new Car();//CAR() is the constructor helps us creat a new
+                                  //instance of the CAR() class; VAR is of type CAR
+                                  //Set values in the properties for the object ((.) Dot Notation way
+            myCar.Make = "Toyota";
+            myCar.Model = "Camery";
+            myCar.Year = 2017;
+            //OR OBJECT INITIALIZER WAY BELOW
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2015
+            };
+            //OR
+            var chevy = new Car("Chevy", "Impala", 1967);
+            //Print the values of each property to the Console
+            var carList = new List<Car>() { myCar, mazda, chevy, };
+            //List<Car> is a constructor
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($" {vehicle.Make}, {vehicle.Make}, {vehicle.Year}");
+            }
+        }
+    
+    }
+}
